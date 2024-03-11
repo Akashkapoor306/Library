0d122fd8b7e819bb2e46fd4c26095225e6c7db4d
AkashKapoor <akashkapoor306@gmail.com>
2024-03-11 06:33:36 -0400

    Added css and make addBookToLibrary functionality

--- script.js
+++ script.js
@@ -1,28 +1,101 @@
-const card = document.getElementsByClassName(".card");
+const dialogelement = document.getElementById("dialog");
+const showButton = document.querySelector(".open");
+const closeButton = document.querySelector('.close');
+const submitButton = document.querySelector('.submit');
+
+
+showButton.addEventListener('click',() => {
+    dialogelement.showModal();
+});
+
+closeButton.addEventListener('click',() =>{
+    dialogelement.close();
+});
+
+
+submitButton.addEventListener('click', addBookToLibrary)
 
 const library = [
-    {name:'Gaming',author:'Akash',pages:120},
-    {name:'Gaming',author:'Akash',pages:120},
-    {name:'Gaming',author:'Akash',pages:120}
+    {Title:'Sky is the Limit',Author:'Akash',pages:120},
+    {Title:'Sky is the Limit',Author:'Akash',pages:120},
+    {Title:'Sky is the Limit',Author:'Akash',pages:120},
 ]
 
 
 
-library.forEach(element => {
-    let div =  document.createElement('div');
-    let name =  document.createElement('h1')
-    let author =  document.createElement('p');
-    let pages = document.createElement('p')
+function renderContainer(libraryArray)
+{
+    const container = document.querySelector(".container");
+    container.innerHTML = '';
 
-    name.textContent =  element.name;
-    author.textContent = element.author;
-    pages.textContent = element.pages
-
-    div.append(name);
-    div.append(author);
-    div.append(pages);
-
-    div.append(card)
+    libraryArray.forEach(element => {
+        
+        const card =  document.createElement('div');
+        card.className = 'card';
 
 
-});
+        const title = document.createElement('h2');
+        title.textContent = `${element.Title}`;
+
+        const author = document.createElement('p');
+        author.textContent = `${element.Author}`;
+
+
+        const pages = document.createElement('p');
+        pages.textContent = `${element.pages}`;
+
+        const readButton = document.createElement('button');
+        readButton.className = 'card-button';
+        readButton.textContent = 'Read'
+
+        const removeButton = document.createElement('button');
+        removeButton.className = 'card-button';
+        removeButton.textContent = 'Remove'
+
+
+
+
+
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages)
+        card.appendChild(readButton);
+        card.appendChild(removeButton);
+
+        container.appendChild(card);
+
+    }); 
+   
+}
+
+
+renderContainer(library)
+
+function book(Title,Author,Pages)
+{
+    this.Title = Title;
+    this.Author = Author;
+    this.Pages = Pages;
+}
+
+
+function addBookToLibrary()
+{
+    const inputTitle = document.getElementById('Title-input')
+    const inputAuthor = document.getElementById('Author-input');
+    const inputPages = document.getElementById('Pages-input')
+    
+
+
+    const newBook = new book(inputTitle.value,inputAuthor.value,inputPages.value)
+
+    library.push({...newBook});
+    renderContainer(library)
+}
+
+
+
+
+
+
+
